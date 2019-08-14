0c475f6315478ea0ca19e57de708e831233b3bdf
Kevin Karma <kevinkarma55@gmail.com>
2019-08-14 15:52:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(short_words)
   return false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
