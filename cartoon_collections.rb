0ff4e9f540a242a0b748f6fd5d37861f3439ead3
Kevin Karma <kevinkarma55@gmail.com>
2019-08-14 14:23:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarfName)
   name = []
   name = dwarfName.sort_by { |name| name.length }
-  name.each { |name| puts name }
+  name.each { |name| puts name + "\n" }
 end
 
 def summon_captain_planet# code an argument here
